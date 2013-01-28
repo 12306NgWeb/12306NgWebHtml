b51665f930150e3f21eb26ed411afaf5970e0e88
imouze <ymk908@163.com>
2013-01-28 10:46:50 +0800

    页面重构
    页面重构

---
+++ branch/js/index.js
@@ -0,0 +1,37 @@
+/*
+*Email:wfengchzu@gmail.com
+*date:2013-01-15
+*modified by: imouze 2013-01-27
+*/
+$().ready(function(){
+	$('.fullscreen').css("height",($(window).height()-40));
+	$('.module-middle').css('position', 'relative')
+					   .css('top', ($(window).height() - $('.module-middle').height()) / 3);
+	
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
